c69b12dea09c54a65e0a730a0f317fcc92950a06
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 14:32:17 +0100

    05._Print_Even_Numbers

---
+++ Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
