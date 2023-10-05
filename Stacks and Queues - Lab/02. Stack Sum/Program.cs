3563f0a3a09a74fd71cd86480c4c23728c1ab54a
Simona Evgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 14:36:48 +0100

    Update Program.cs

--- Stacks and Queues - Lab/02. Stack Sum/Program.cs
+++ Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace _02._Stack_Sum
+namespace _01._Stack_and_Queues
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string command = Console.ReadLine().ToLower();
+
+            while ( command != "end")
+            {
+                string[] commandInfo = command.Split();
+
+                if (commandInfo[0] == "add")
+                {
+                    int number = int.Parse(commandInfo[1]);
+                    int number1 = int.Parse(commandInfo[2]);
+                    stack.Push(number);
+                    stack.Push(number1);
+
+                }
+                else
+                {
+                    int number = int.Parse(commandInfo[1]);
+
+                    if (stack.Count >= number)
+                    { 
+                        for (int i = 0; i < number; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                   
+                   
+
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+
         }
     }
 }
