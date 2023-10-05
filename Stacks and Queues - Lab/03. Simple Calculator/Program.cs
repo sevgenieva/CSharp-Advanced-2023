465032a15b6e234140dfb967694fc50702784a4e
Simona Evgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 14:37:07 +0100

    Update Program.cs

--- Stacks and Queues - Lab/03. Simple Calculator/Program.cs
+++ Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace _03._Simple_Calculator
+namespace _03._Simple_Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> list = Console.ReadLine().Split().ToList(); ;
+           Stack<string> info = new Stack<string>(list);
+            Stack<string> reverse = new Stack<string>();
+
+            foreach (var line in info)
+            {
+                reverse.Push(line);
+            }
+            
+            int sum = 0;
+            int number = 0;
+            string info1 = string.Empty;
+
+            while (reverse.Count > 0)
+            {
+                info1 = reverse.Pop();
+
+                if (info1 == "+")
+                {
+                    number = int.Parse(reverse.Pop());
+                    sum += number;
+                }
+                else if (info1 == "-")
+                {
+                    number = int.Parse(reverse.Pop());
+                    sum -= number;
+                }
+                else
+                {
+                    sum = int.Parse(info1);
+                }
+                
+
+
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
