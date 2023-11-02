7dc577db6c1367378e3f727208c1f99d327e4a5f
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-11-02 12:34:35 +0000

    solution

---
+++ C# OOP/Inheritance/02. Multiple Inheritance/Dog.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Farm
+{
+  public class Dog :Animal
+    {
+        public void Bark()
+        {
+
+            Console.WriteLine("barking…");
+        }
+    }
+}
---
+++ C# OOP/Inheritance/02. Multiple Inheritance/Puppy.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Farm
+{
+    public class Puppy :Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping…");
+        }
+
+    }
+}
